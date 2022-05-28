90a7acd8079339d872bf09c7f7c4708328111a50
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-05-28 12:10:24 +0200

    dsgfnjksdngfjk fsvgsfdg dsgds

--- queries/UserQueries.js
+++ queries/UserQueries.js
@@ -3,5 +3,5 @@ export const UserQueries = {
   modifyUser: `UPDATE users SET name = ?, pass = ?, position = ? WHERE id = ?`,
   getUsers: `SELECT * FROM users`,
   findUserByName: `SELECT id,name FROM users WHERE name = ?`,
-  dummyLogin: `SELECT COUNT(name) as succeed, id as userId, name as userName, FROM users WHERE name=? AND pass=?`,
+  dummyLogin: `SELECT COUNT(name) as succeed, id as userId, name as userName FROM users WHERE name=? AND pass=?`,
 };
